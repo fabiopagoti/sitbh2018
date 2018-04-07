d7052889ca8f89f8fa3632cb8443a829a44db431
Fábio Pagoti <fabiopagoti@gmail.com>
2018-04-07 07:09:45 +0000

    salvar e cancelar implementados

--- webapp/controller/NovaCategoria.controller.js
+++ webapp/controller/NovaCategoria.controller.js
@@ -1,12 +1,18 @@
 sap.ui.define([
-	"sitbh/controller/BaseController"
-], function(BaseController) {
+	"sitbh/controller/BaseController",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox"
+], function(BaseController, MessageToast, MessageBox) {
 	"use strict";
 
 	return BaseController.extend("sitbh.controller.NovaCategoria", {
 
 		onInit: function() {
 			this.getRouter().getRoute("novaCategoria").attachPatternMatched(this._onPatternMached, this);
+		},
+
+		onNavBack: function() {
+			this.getRouter().navTo("default");
 		},
 
 		_onPatternMached: function(oEvent) {
@@ -16,17 +22,33 @@ sap.ui.define([
 			var oPromiseMetadataLoaded = this.getModel().metadataLoaded();
 			oPromiseMetadataLoaded.then(function() {
 
-				var oContext = oModel.createEntry("/Categories", {
-
+				this._oContext = oModel.createEntry("/Categories", {
 				});
 
-				this.getView().setBindingContext(oContext);
+				this.getView().setBindingContext(this._oContext);
 
 			}.bind(this));
 		},
 
 		onSave: function() {
-			this.getModel().submitChanges();
+			
+			
+			function onSuccess(oNovaCategoria){
+				MessageToast.show("Coleção cadastrada com sucesso");
+			}
+			
+			function onError(oError){
+				MessageBox.error("Erro na criação da categoria");
+			}
+			
+			this.getModel().submitChanges({
+				success: onSuccess.bind(this),
+				error: onError.bind(this)
+			});
+		},
+		
+		onCancel: function(){
+			this.getModel().deleteCreatedEntry(this._oContext);
 		}
 	});
 
